6644254e72a50b79253e377a1093b70bc5b70f69
Solveig Alda <solald@gmail.com>
2024-04-21 16:01:11 +0000

    jic commit

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -16,6 +16,8 @@ const NotFound: React.FC = () => {
             <div>                
                 <h1>oh no!</h1>
                 <h1>There is no internet</h1>
+                <h2>It&apos;s a good idea to have an offline page because I might not be precaching 
+                    everything and you don&apos;t want your users to get stuck on a blank screen.</h2>
             </div> 
         </div>
     )
